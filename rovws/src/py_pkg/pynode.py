9c9a9cb88b36a961c448d4fe831a9d829fc6b00f
vonFoscheptik <furkan.karstarli@std.yildiz.edu.tr>
2024-03-14 14:33:22 +0300

    iki dilde birer node oluşturuldu
    bunlardan python bizim ardsub ile iletişimimiz için geliştirielecek
    
    cpp ise istasyon(bilgisayar) ve raspberry pi arası iletişimde kullanılacak

---
+++ rovws/src/py_pkg/pynode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+ 
+ 
+class MyCustomNode(Node): # class declaretion for node
+    def __init__(self):
+        super().__init__("node_name") # node initial object attributes declaretion
+ 
+ 
+def main(args=None): #main function
+    rclpy.init(args=args)
+    node = MyCustomNode() # node attended to "node" variable
+    rclpy.spin(node) #prevent the close terminal after node functions have done
+    rclpy.shutdown()
+ 
+ 
+if __name__ == "__main__":
+    main()
